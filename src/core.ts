d4726de8e4b8fc593e2c77a2b96b0ec5fc95addd
isoden <isoda@maboroshi.biz>
2017-07-30 04:06:00 +0900

    chore: use got instead of axios

--- src/core.ts
+++ src/core.ts
@@ -1,21 +1,20 @@
 'use strict'
 
-import axios from 'axios'
+import * as got from 'got'
 
 export const search = (searchParams: TypeSearchCli.TypeSearchParams): Promise<TypeSearchCli.NpmSearchResponse> => {
-  const params  = normalizeParams(searchParams)
+  const query: Partial<TypeSearchCli.NpmSearchParams>  = {
+      text       : `scope:types ${ searchParams.text }`,
+      quality    : 1.95,
+      popularity : 3.3,
+      maintenance: 2.05,
+    }
 
-  return axios.get(`https://www.npmjs.org/-/search`, { params })
-    .then(response => response.data as TypeSearchCli.NpmSearchResponse)
-}
-
-export const normalizeParams = (params: TypeSearchCli.TypeSearchParams): TypeSearchCli.NpmSearchParams => {
-  return {
-    text       : `scope:types ${ params.text }`,
-    quality    : 1.95,
-    popularity : 3.3,
-    maintenance: 2.05,
-  }
+  return got(`https://www.npmjs.org/-/search`, {
+    query,
+    json: true,
+  })
+    .then(response => <TypeSearchCli.NpmSearchResponse>response.body)
 }
 
 export namespace TypeSearchCli {
@@ -26,22 +25,22 @@ export namespace TypeSearchCli {
   /** {@link https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md#get-v1search} */
   export interface NpmSearchParams {
     /** full-text search to apply */
-    text?: string
+    text: string
 
     /** how many results should be returned (default 20, max 250) */
-    size?: number
+    size: number
 
     /** offset to return results from */
-    from?: number
+    from: number
 
     /** how much of an effect should quality have on search results */
-    quality?: number
+    quality: number
 
     /** how much of an effect should popularity have on search results */
-    popularity?: number
+    popularity: number
 
     /** how much of an effect should maintenance have on search results */
-    maintenance?: number
+    maintenance: number
   }
 
   export interface NpmMaintainer {
